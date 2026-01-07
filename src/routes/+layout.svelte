60335088e8823c95140d6bd0e9ddff02ebf321f3
Mijo Majic <mmajic@jaspero.co>
2026-01-07 12:52:12 +0100

    feat: Initialize SvelteKit project with base HTML template and global layout component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,9 @@
 	import './layout.css';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="flex min-h-screen flex-col">
 	<Navbar />
